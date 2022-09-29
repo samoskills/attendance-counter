3d93568a0d0ad30a1d2bb81e392f0777a3a0daf7
Samson Nnah <112515308+samoskills@users.noreply.github.com>
2022-09-29 11:41:37 +0100

    This is the first version of the AC app
    More improvements and updates will follow:
    1. Date and time
    2. Export data as CSV file 
    etc

---
+++ index.js
@@ -0,0 +1,44 @@
+let dateEl = document.getElementById("date-el")
+let saveManEl = document.getElementById("save-man-el")
+let saveWomanEl = document.getElementById("save-woman-el")
+let saveChildEl = document.getElementById("save-children-el")
+let countManEl = document.getElementById("count-el")
+let countWomanEl = document.getElementById("count-el")
+let countChildEl = document.getElementById("count-el")
+let countM = 0
+let countW = 0
+let countC = 0
+
+function date() {
+    dateEl.innerHTML(new Date()) 
+}
+function man() {
+    countM += 1
+    countManEl.textContent = countM
+}
+
+function woman() {
+    countW += 1
+    countWomanEl.textContent = countW
+}
+
+function child() {
+    countC += 1
+    countChildEl.textContent = countC
+
+}
+
+function save() {
+    let countMStr = countM + " - "
+    let countWStr = countW + " - "
+    let countCStr = countC + " - "
+    saveManEl.textContent += countMStr
+    saveWomanEl.textContent += countWStr
+    saveChildEl.textContent += countCStr
+    countManEl.textContent = 0
+    countWomanEl.textContent = 0
+    countChildEl.textContent = 0
+    countM = 0
+    countW = 0
+    countC = 0
+}
